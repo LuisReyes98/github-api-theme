d6a2ffbac9c1d7f469e777eb3d9efb81b0ad674e
Luis Reyes <luisrogelio98@gmail.com>
2019-06-07 21:24:55 -0400

    pagination visibility error fixed

--- javascript/tabulator_table.js
+++ javascript/tabulator_table.js
@@ -2,6 +2,7 @@
 
 let $tableId = "#{{issue_table_id}}";
 let $tabulatorWarning = document.getElementById("entries_warning");
+let $pagination = document.getElementById("issues_table_pagination");
 
 let server_response = {{ server_response | safe }};
 
@@ -24,14 +25,11 @@ if (typeof (server_response) !== "string"){
     let table = new Tabulator($tableId, {
       data: server_response,
       // pagination: "local", //enable local pagination.
-
-      paginationSize: 10, //ammount of elements per page  
-      // paginationSizeSelector: true, //amount of elements per page 
+      paginationSize: 10, //ammount of elements per page
+      // paginationSizeSelector: true, //amount of elements per page
       tooltips: true,            //show tool tips on cells
-
-
       // autoResize: true,
-      // movableColumns: true, //allow column order to be changed  
+      // movableColumns: true, //allow column order to be changed
       // layout: "fitColumns",
       columns: Object.keys(server_response[0]).map(function (item) {
         /*for each key values in the first element assuming all elemnents behave the same
@@ -44,29 +42,28 @@ if (typeof (server_response) !== "string"){
           headerFilter: "input",
           headerFilterPlaceholder: `Filter ${item}`,
           headerFilterFunc: "like",
-          formatter: function (cell, formatterParams, onRendered) {        
+          formatter: function (cell, formatterParams, onRendered) {
             /*
               cell - the cell component
               formatterParams - parameters set for the column
-              onRendered - function to call when the formatter has been rendered            
-            
-              adding the attribute to each cell with the same 
+              onRendered - function to call when the formatter has been rendered
+              adding the attribute to each cell with the same
               value as the title of the column header in
               for that cell,this is used so the custom css can show the column name when needed
             */
-            cell.getElement().setAttribute("title_of_data", cell._cell.column.definition.title); 
+            cell.getElement().setAttribute("title_of_data", cell._cell.column.definition.title);
             return this.emptyToSpace(this.sanitizeHTML(cell.getValue()));
-          },            
+          },
         }
-      })      
+      })
     });
     // function to reload the table when the browser window changes size
     window.addEventListener('resize', function () {
       table.redraw(true); //reloading table visuals
     });
-    
   } else{
     $tabulatorWarning.classList.remove("hidden");
+    $pagination.classList.add("hidden");
   }
 
 }
